cd3162f59ef86da60d1edc6782e9462c39a3c347
reginaldo <regcostajr@gmal.com>
2017-05-08 09:46:19 -0300

    import http

--- services/send.go
+++ services/send.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/astarlabs/bitcoin-go-api-client/dto/params"
 	"github.com/astarlabs/bitcoin-go-api-client/dto/result"
+	"github.com/astarlabs/bitcoin-go-api-client/http"
 
 	"strconv"
 
